1c1f3a545656dd670f3e15216bb3b89180d2b509
0xdevin <devin@nirvanalabs.ai>
2023-01-06 15:20:45 -0800

    add txn block

--- components/Web3/LatestTxns.jsx
+++ components/Web3/LatestTxns.jsx
@@ -1,28 +1,52 @@
 import React from "react";
 import useSWR from "swr";
 import { getEllipsisTxt } from "../../utils/helpers";
+import { ethers } from "ethers";
 
 const fetcher = (url) => fetch(url).then((res) => res.json());
 
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
+
 export default function LatestTxns() {
   const { data, error } = useSWR(
-    "https://api.nirvanalabs.xyz/txns/latest",
+    "https://api.nirvanalabs.xyz/txns/latest?size=100",
     fetcher
   );
   if (error) return "An error has occurred.";
   if (!data) return "Loading...";
   return (
-    <div className="flex text-sm flex-col bg-black opacity-70 m-5 p-5 rounded-lg">
+    <div className="flex text-sm flex-col bg-black opacity-60 m-5 p-5 rounded-xl">
       <h2 className="text-xl font-fira font-semibold mb-2">Latest ETH Txns</h2>
       {data.map((txn) => {
         return (
           <div
             key={txn.transaction_hash}
-            className="w-full flex justify-start my-0.5 py-1 px-4 bg-slate-900 rounded"
+            className={classNames(
+              txn.type == "ERC721"
+                ? "w-full flex justify-between cursor-pointer my-1 py-1 px-4 bg-slate-900 rounded"
+                : "w-full flex justify-between cursor-pointer my-1 py-1 px-4 bg-slate-800 rounded"
+            )}
           >
             <div className="flex-col">
               <div className=" text-md">{txn.name}</div>
               <div> {getEllipsisTxt(txn.address)}</div>
+              <div className="text-xs mt-2">{txn.type}</div>
+            </div>
+            <div>
+              {txn.type == "ERC20" ? (
+                <span>
+                  {" "}
+                  {String(ethers.utils.formatEther(txn.value)).substring(
+                    0,
+                    4
+                  )}{" "}
+                  ETH
+                </span>
+              ) : (
+                <span># {txn.value}</span>
+              )}
             </div>
           </div>
         );
---
+++ components/Web3/TxnBlock.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function TxnBlock() {
+  return <div>TxnBlock</div>;
+}
